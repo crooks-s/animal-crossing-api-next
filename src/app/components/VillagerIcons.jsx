b96bdacf812f42e9754a59db495d20afc71bf556
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-03-13 13:36:18 -0700

    fix: routing directories

---
+++ src/app/components/VillagerIcons.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Image from "next/image";
+
+const VillagerIcons = (props) => {
+  const { className, alt, species, personality, sign } = props;
+
+  const importAll = (r) => {
+    let images = {};
+    r.keys().forEach((key) => (images[key] = r(key).default));
+    return images;
+  };
+
+  const speciesImages = importAll(
+    require.context("../../../public/images/species", false, /\.(png|jpe?g|svg)$/)
+  );
+  const signsImages = importAll(
+    require.context("../../../public/images/signs", false, /\.(png|jpe?g|svg)$/)
+  );
+  const personalitiesImages = importAll(
+    require.context(
+      "../../../public/images/personalities",
+      false,
+      /\.(png|jpe?g|svg)$/
+    )
+  );
+
+  const handleSrc = (species, personality, sign) => {
+    if (species) {
+      const speciesLower = species.toLowerCase(); // lower case d/t file naming convention in lower case
+      const speciesDashes = speciesLower.replace(" ", "-"); // account for 'bear cub' species
+      const speciesSrc = speciesImages[`./${speciesDashes}.png`];
+      if (!speciesSrc) {
+        return speciesImages[`./special.png`].src; // default to special species
+      }
+      return speciesSrc.src;
+    }
+    if (personality) {
+      const personalityLower = personality.toLowerCase(); // lower case d/t file naming convention in lower case
+      const personalityDashes = personalityLower.replace(" ", "-"); // account for 'big sister' personality
+      const personalitySrc = personalitiesImages[`./${personalityDashes}.png`];
+      if (!personalitySrc) {
+        return personalitiesImages[`./normal.png`]; // default to normal personality
+      }
+      return personalitySrc.src;
+    }
+    if (sign) {
+      const signLower = sign.toLowerCase(); // lower case d/t file naming convention in lower case
+      const signSrc = signsImages[`./${signLower}.png`];
+      return signSrc.src;
+    }
+    return null;
+  };
+
+  return (
+    <Image
+      className={className}
+      src={handleSrc(species, personality, sign)}
+      alt={alt}
+      height={36}
+      width={36}
+    />
+  );
+};
+
+export default VillagerIcons;
---
+++ src/pages/villager/loading.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <main className="bg-hero-pattern bg-center h-screen bg-no-repeat bg-cover flex items-center justify-center">
+      {/* top level card -- card background */}
+      <div className="h-4/5 w-3/5 rubik flex flex-col items-center py-0 px-0 bg-cover pt-3 bg-card-pattern shadow-2xl rounded-3xl ">
+        <span className="relative -left-32 top-14">Loading name...</span>
+        {/* quote box */}
+        <div className="h-32 w-full bg-chat-pattern bg-center bg-no-repeat bg-contain flex items-center justify-center mt-10">
+          <p className="">Loading quote...</p>
+        </div>
+        {/* species */}
+        <div className="items-center mt-5 rounded-xl border-8 box-border border-[#ec9900] h-16 w-3/4 max-w-96 flex flex-row bg-[#ffa500]">
+          <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
+            Species
+          </span>
+          Loading species...
+        </div>
+        {/* personality */}
+        <div className="items-center mt-5 rounded-xl border-8 box-border border-[#2e6b7b] h-16 w-3/4 max-w-96 flex flex-row bg-[#107896]">
+          <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
+            Personality
+          </span>
+          Loading personality...
+        </div>
+        {/* hobby */}
+        <div className="items-center mt-5 rounded-xl border-8 box-border border-[#7f3d35] h-16 w-3/4 max-w-96 flex flex-row bg-[#983f34]">
+          <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
+            Sign
+          </span>
+          Loading sign...
+        </div>
+        {/* birthday */}
+        <div className="items-center mt-5 rounded-xl border-8 box-border border-[#6f8141] h-16 w-3/4 max-w-96 flex flex-row bg-[#829356]">
+          <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
+            Birthday
+          </span>
+          <p>Loading birthday...</p>
+        </div>
+        {/* avatar */}
+        <div className="pt-10">Loading image...</div>
+      </div>
+    </main>
+  );
+};
+
+export default Loading;
