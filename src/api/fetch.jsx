6248bfc84ea66fbf549635b39041823960b94035
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-03-13 15:22:18 -0700

    add error handling, comments

--- src/api/fetch.jsx
+++ src/api/fetch.jsx
@@ -3,14 +3,19 @@ export default async function fetchData(apiKey) {
   const fetchOptions = {
     method: "GET",
     headers: {
-      'x-api-key': apiKey
+      'x-api-key': apiKey,
     }
   }
+
   try {
     const response = await fetch(`https://api.nookipedia.com/villagers`, fetchOptions);
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.error("Error fetching data: ", err);
+    if (response.ok) { // if HTTP-status is 200-299, found the villager
+      const data = await response.json();
+      return data;
+    } else { // if HTTP-status is not 200-299, return the status
+      return response.status;
+    }
+  } catch (err) { // if fetch fails, return 500
+    return 500;
   }
 }; 
