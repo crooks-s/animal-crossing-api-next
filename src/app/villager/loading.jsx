21cea79dd1f9987cf034dbdecca9a6fd71917459
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-03-13 15:35:32 -0700

    edit styling

--- src/app/villager/loading.jsx
+++ src/app/villager/loading.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <main className="bg-hero-pattern bg-center h-screen bg-no-repeat bg-cover flex items-center justify-center">
+    <main className="overflow-y-auto bg-hero-pattern bg-center min-h-screen bg-no-repeat bg-cover flex items-center justify-center">
       {/* top level card -- card background */}
       <div className="h-4/5 w-3/5 rubik flex flex-col items-center py-0 px-0 bg-cover pt-3 bg-card-pattern shadow-2xl rounded-3xl ">
-        <span className="relative -left-32 top-14">Loading name...</span>
+        {/* name */}
+        <span className="text-white h-auto -mt-12 py-2 px-3 relative top-20 rounded-full bg-[#c0503c]">Loading name...</span>
         {/* quote box */}
         <div className="h-32 w-full bg-chat-pattern bg-center bg-no-repeat bg-contain flex items-center justify-center mt-10">
           <p className="">Loading quote...</p>
         </div>
+        {/* avatar */}
+        <div className="pt-10">Loading villager avatar...</div>
         {/* species */}
         <div className="items-center mt-5 rounded-xl border-8 box-border border-[#ec9900] h-16 w-3/4 max-w-96 flex flex-row bg-[#ffa500]">
           <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
@@ -32,14 +35,12 @@ const Loading = () => {
           Loading sign...
         </div>
         {/* birthday */}
-        <div className="items-center mt-5 rounded-xl border-8 box-border border-[#6f8141] h-16 w-3/4 max-w-96 flex flex-row bg-[#829356]">
+        <div className="items-center mb-20 mt-5 rounded-xl border-8 box-border border-[#6f8141] h-16 w-3/4 max-w-96 flex flex-row bg-[#829356]">
           <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
             Birthday
           </span>
           <p>Loading birthday...</p>
         </div>
-        {/* avatar */}
-        <div className="pt-10">Loading image...</div>
       </div>
     </main>
   );
