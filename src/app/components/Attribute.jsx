8de10d15cb27e1e0f8d7ede7e41e80d23c48cde7
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-03-14 13:22:04 -0700

    add conditionals to render sign, personality

--- src/app/components/Attribute.jsx
+++ src/app/components/Attribute.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import VillagerIcon from "./VillagerIcon";
 
-const Attribute = ({ title, villager, colorScheme, iconColor }) => {
+const Attribute = ({ title, villager, colorScheme, iconColor, addBorder }) => {
   const lowerTitle = title.toLowerCase();
   const attribute = villager[lowerTitle];
-  // console.log(attribute);
 
   return (
     <div className={`items-center mt-5 rounded-xl border-8 box-border h-16 w-3/4 max-w-96 flex flex-row ${colorScheme}`}>
       <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
-        Species
+        {title}
       </span>
       <span className="bg-[#fdf8e7] w-3/4 h-full rounded-lg shadow-2xl text-center flex-grow flex items-center justify-start">
         <div className="flex-shrink-0 ml-2 mr-2">
           {villager && attribute ? (
             <VillagerIcon
-              className={`rounded-full border-2 ${iconColor}`}
-              species={attribute}
+              className={`rounded-full ${addBorder} ${iconColor}`}
+              species={title === "Species" ? attribute : null}
+              personality={title === "Personality" ? attribute : null}
+              sign={title === "Sign" ? attribute : null}
               alt={`${title} icon`}
             />
           ) : null}
