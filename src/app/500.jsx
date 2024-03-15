10ba1a88dc9d83511efcf27d759dd1a702d903ad
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-03-15 09:53:23 -0700

    add town hall image and Home btn

--- src/app/500.jsx
+++ src/app/500.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import Image from 'next/image';
+"use client";
+import React from "react";
+import Image from "next/image";
 import TownHall from "../../public/images/town-hall.jpg";
+import { redirect, useRouter } from "next/navigation";
 
 const Custom500 = () => {
+  const router = useRouter();
+
   return (
     <main className="bg-hero-pattern bg-center h-screen bg-no-repeat bg-cover flex items-center justify-center">
       {/* top level card -- card background */}
@@ -10,16 +14,21 @@ const Custom500 = () => {
         <span className="text-center text-xl mt-4 py-4 px-3 rounded-full bg-[#fdf8e7]  ">
           Sorry! Something went wrong! Please try again later.
         </span>
-      <Image 
-      className="border-2 bg-[#fdf8e7] rounded-3xl my-5"
-      src={TownHall}
-      alt='picture of the town hall'
-      height={500}
-      width={500}
-      />
+        <Image
+          className="border-2 bg-[#fdf8e7] rounded-3xl my-5"
+          src={TownHall}
+          alt="picture of the town hall"
+          height={500}
+          width={500}
+        />{" "}
+        <button 
+        onClick={() => router.push('/')}
+        className="bg-[#503cce] hover:bg-[#8098ef] transition ease-in-out py-2 my-6 px-3 flex items-center justify-center rounded-3xl text-white font-normal border-2 border-[#22399e]">
+          Home
+        </button>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Custom500
+export default Custom500;
