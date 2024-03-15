0323479de61f035ed6f6061ba3f1dce12c423fe1
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-03-15 09:56:22 -0700

    add/use Name, ChatBubble, Birthday components

--- src/app/villager/page.jsx
+++ src/app/villager/page.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Image from "next/image";
 const apiKey = process.env.NEXT_PUBLIC_NOOKIPEDIA_API_KEY;
 import fetchData from "../../api/fetch";
-import bday from "../../../public/images/birthday/bday-cake.png";
 import placeholder from "../../../public/images/placeholder.png";
 import NewVillagerBtn from "../components/NewVillagerBtn";
 import Attribute from "../components/Attribute";
+import ChatBubble from "../components/ChatBubble";
+import Name from "../components/Name";
+import Birthday from "../components/Birthday";
 import Custom404 from "../404";
 import Custom500 from "../500";
 
 const Villager = async () => {
   let villager;
+  let bgColor;
 
   try {
     const data = await fetchData(apiKey);
@@ -19,46 +22,48 @@ const Villager = async () => {
     }
     const randomIndex = Math.floor(Math.random() * data.length);
     villager = data[randomIndex];
+    bgColor = villager.title_color || 'e06c2d';
   } catch (err) {
     return <Custom500 />;
   }
 
   return (
     // background image
-    <main className="overflow-y-auto bg-hero-pattern bg-center min-h-screen bg-no-repeat bg-cover flex items-center justify-center">
+    <main className="overflow-y-auto bg-hero-pattern bg-center min-h-screen bg-no-repeat bg-cover flex items-center justify-center pt-2">
       {/* top level card -- card background */}
       <div className=" h-auto w-3/5 rubik flex flex-col items-center py-0 px-0 fixed-bg bg-cover bg-card-pattern shadow-2xl rounded-3xl  min-h-[632px] max-w-2xl min-w-96 animate-ease-in">
-        
         {/* Headshot */}
         <div className="rounded-full">
-
-            <Image
-              className="rounded-full bg-[#fdf8e7] p-2 m-0 -mt-6 -mb-16 shadow-2xl "
-              src={villager.nh_details ? villager.nh_details.icon_url || placeholder : placeholder}
-              alt="headshot photo"
-              height={100}
-              width={100}
-            />
-
+          <Image
+            className="rounded-full bg-[#fdf8e7] p-2 m-0 -mt-6 -mb-16 shadow-2xl "
+            src={
+              villager.nh_details
+                ? villager.nh_details.icon_url || placeholder
+                : placeholder
+            }
+            alt="headshot photo"
+            height={100}
+            width={100}
+          />
         </div>
 
         {/* name + name background */}
-        <div className="h-auto -mt-12 py-2 px-3 relative -left-28 -rotate-12 top-20 rounded-full bg-[#c0503c]">
-          {/* villager name */}
-          <span className="relative text-white">
-            {villager && villager.name ? villager.name : "Unknown"}
-          </span>
-        </div>
+        <Name
+          villager={villager}
+          style={{
+            backgroundColor: `#${bgColor}`,
+            color: `#${villager.text_color}`,
+          }}
+        />
 
         {/* Chat Bubble */}
-        <div className="min-h-[10rem] w-full bg-chat-pattern bg-center bg-no-repeat bg-contain flex items-center justify-center mt-10 p-0 -z-10">
-          {/* //TODO: add ternary in case of null value */}
-          <span className="flex items-center justify-center w-1/2">
-            {villager && villager.quote
-              ? villager.quote
-              : "This villager does not have a particular saying!"}
-          </span>
-        </div>
+        <ChatBubble
+          villager={villager}
+          style={{
+            backgroundColor: `#${villager.title_color}`,
+            color: `#${villager.text_color}`,
+          }}
+        />
 
         {/* Species */}
         <Attribute
@@ -95,31 +100,13 @@ const Villager = async () => {
         />
 
         {/* Birthday */}
-        <div className="items-center mt-2 rounded-xl border-8 box-border border-[#6f8141] h-16 w-3/4 max-w-96 flex flex-row bg-[#829356]">
-          <span className="font-normal text-white h-10 w-1/3 flex items-center justify-center">
-            Birthday
-          </span>
-          <span className="bg-[#fdf8e7] w-3/4 h-full rounded-lg shadow-lg text-center flex-grow flex items-center justify-start ">
-            <div className="flex-shrink-0 ml-2 mr-2">
-                <Image
-                  className="rounded-full bg-white"
-                  src={bday}
-                  alt="birthday cake icon"
-                  height={36}
-                  width={36}
-                />
-            </div>
-            {villager && villager.birthday_month && villager.birthday_day
-              ? `${villager.birthday_month} ${villager.birthday_day}`
-              : "Unknown"}
-          </span>
-        </div>
+        <Birthday villager={villager} />
 
         {/* Avatar */}
         <div className="pt-0 -mt-[15px]">
           {villager && villager.image_url ? (
             <Image
-              className="z-10 mt-10"
+              className="z-10 mt-10 ease-in-out"
               src={villager.image_url}
               alt={`image of {villager.name}`}
               height={100}
